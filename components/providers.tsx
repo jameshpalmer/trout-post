d7835e752f00018903ca772c2fb41e91684476c1
jameshpalmer <jameshpalmer0@gmail.com>
2023-08-02 00:41:58 +0100

    Add Vercel Analytics component

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,14 +1,18 @@
 "use client"
 
 import * as React from "react"
+import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 import { FishProvider } from "./fish-toggle"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      <FishProvider>{children}</FishProvider>
-    </NextThemesProvider>
+    <>
+      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+        <FishProvider>{children}</FishProvider>
+      </NextThemesProvider>
+      <Analytics />
+    </>
   )
 }
